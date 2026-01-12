9c6b4a3345a906974cbf286c91e4607458afd992
Fnordgoose3 <Biel_machado08@hotmail.com>
2026-01-11 23:48:43 -0300

    DOM and logic

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 console.log("JS conectado!");
+
+const input = document.getElementById('taskInput');
+const button = document.getElementById('addBtn');
+const list = document.getElementById('taskList');
+
+button.addEventListener('click', addTask);
+
+function addTask(){
+    const text = input.ariaValueMax.trim();
+
+    if (text === ""){
+        alert("Digite alguma tarefa");
+        return;
+    }
+
+    const li = document.createElement('li');
+    li.textContent = text;
+    list.appendChild(li);
+
+    input.value = "";
+
+    input.addEventListener('keypress', function(e) {
+    if (e.key === "Enter") {
+        addTask();
+    }
+});
+
+
+}
